042cf205015e36674bb7e1a4b23289cd646f8879
Daniel Karski <daniel.karski5q@gmail.com>
2020-09-03 15:41:09 +0200

    Added log in @oryginal/to-collection-tiny

--- packages/to-collection-tiny/index.js
+++ packages/to-collection-tiny/index.js
@@ -3,6 +3,6 @@ const toCapitalCase = require("@oryginal/to-capital-case")
 
 const capitalizeItems = (items) => items.map(toCapitalCase)
 
-const toCollection = (items) => toUnique(capitalizeItems(items))
+const toCollection = (items) => console.log(toUnique(capitalizeItems(items)))
 
 module.exports = toCollection
