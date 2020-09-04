eb2551e49d6eee9ca058ff7d24bab609bc6de58a
Daniel Karski <daniel.karski5q@gmail.com>
2020-09-04 08:04:33 +0200

    Added types field in package.json

--- packages/to-collection/index.ts
+++ packages/to-collection/index.ts
@@ -4,6 +4,7 @@ import toCapitalCase from "@oryginal/to-capital-case"
 
 const capitalizeItems = (items: string[]) => items.map(toCapitalCase)
 
-const toCollection = (items: string[]) => compose(toUnique, capitalizeItems)(items)
+const toCollection = (items: string[]) =>
+  compose(toUnique, capitalizeItems)(items)
 
 export default toCollection
